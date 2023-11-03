cdef87626d104ddf0d17f903c8096bad11834f8c
nitagr <nitishagrahari12@gmail.com>
2023-11-03 21:47:53 +0530

    fix: driectory for bin change

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-go build -o ../bin/geektrust
-../bin/geektrust sample_input/input1.txt
-../bin/geektrust sample_input/input2.txt
+go build -o ../../bin/geektrust
+../../bin/geektrust sample_input/input1.txt
+../../bin/geektrust sample_input/input2.txt
